e6ddfbe4a569d96f9d89d284fa0a1a9f7adc949d
XadillaX <admin@xcoder.in>
2015-12-20 00:03:59 +0800

    create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * XadillaX created at 2015-12-19 23:48:21 With ♥
+ *
+ * Copyright (c) 2015 Souche.com, all rights
+ * reserved.
+ */
+"use strict";
+
+module.exports = {
+    Consumer: require("./lib/consumer"),
+    Producer: require("./lib/producer")
+};
