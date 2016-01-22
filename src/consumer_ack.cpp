bd64a4cce982ca334b92e1713059684f770819fe
XadillaX <admin@xcoder.in>
2016-01-22 19:22:59 +0800

    add consumer ack object

---
+++ src/consumer_ack.cpp
@@ -0,0 +1,42 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  consumer_ack.cpp
+ *
+ *    Description:  ONS consumer ACK wrapper for node.js
+ *
+ *        Version:  1.0
+ *        Created:  2016/01/25 11时37分20秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  XadillaX (ZKD), zhukaidi@souche.com
+ *   Organization:  Design & Development Center, Souche Car Service Co., Ltd, HANGZHOU
+ *
+ * =====================================================================================
+ */
+#include "consumer_ack.h"
+
+std::string ack_env_v = std::getenv("NODE_ONS_LOG") == NULL ?
+        "" : std::getenv("NODE_ONS_LOG");
+
+ONSConsumerACKV8::ONSConsumerACKV8()
+{
+}
+
+ONSConsumerACKV8::~ONSConsumerACKV8()
+{
+}
+
+void ONSConsumerACKV8::Done(const Nan::FunctionCallbackInfo<v8::Value>& info)
+{
+    ONSConsumerACKV8* ack = ObjectWrap::Unwrap<ONSConsumerACKV8>(info.Holder());
+    bool succ = true;
+    if(info.Length() >= 1)
+    {
+        succ = info[0]->ToBoolean()->Value();
+    }
+
+    Action act = succ ? Action::CommitMessage : Action::ReconsumeLater;
+    ack->Ack(act);
+}
---
+++ src/consumer_ack.h
@@ -0,0 +1,110 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  consumer_ack.h
+ *
+ *    Description:  ONS consumer ACK wrapper for node.js
+ *
+ *        Version:  1.0
+ *        Created:  2016/01/25 11时16分17秒
+ *       Revision:  none
+ *       Compiler:  g++
+ *
+ *         Author:  XadillaX (ZKD), zhukaidi@souche.com
+ *   Organization:  Design & Development Center, Souche Car Service Co., Ltd, HANGZHOU
+ *
+ * =====================================================================================
+ */
+#ifndef __ONS_CONSUMER_ACK_H__
+#define __ONS_CONSUMER_ACK_H__
+#include <uv.h>
+#include <nan.h>
+#include <string>
+#include <cstdlib>
+#include "Action.h"
+using namespace std;
+
+extern std::string ack_env_v;
+
+class ONSConsumerACKInner {
+public:
+    ONSConsumerACKInner()
+    {
+        uv_cond_init(&cond);
+        uv_mutex_init(&mutex);
+    }
+
+    ~ONSConsumerACKInner()
+    {
+        uv_mutex_destroy(&mutex);
+        uv_cond_destroy(&cond);
+    }
+
+public:
+    void Ack(Action result = Action::CommitMessage)
+    {
+        ack_result = result;
+        if(ack_env_v == "true") printf(">>>>> ACKed: 0x%X\n", this);
+
+        uv_mutex_lock(&mutex);
+        uv_cond_signal(&cond);
+        uv_mutex_unlock(&mutex);
+    }
+
+    Action WaitResult()
+    {
+        uv_mutex_lock(&mutex);
+        uv_cond_wait(&cond, &mutex);
+        uv_mutex_unlock(&mutex);
+
+        Action result = ack_result;
+        if(ack_env_v == "true") printf(">>>>> Finish Wait: 0x%X\n", this);
+
+        return result;
+    }
+
+
+private:
+    uv_mutex_t mutex;
+    uv_cond_t cond;
+    Action ack_result;
+};
+
+class ONSListenerV8;
+class ONSConsumerACKV8 : public Nan::ObjectWrap {
+public:
+    friend class ONSConsumerV8;
+
+private:
+    explicit ONSConsumerACKV8();
+    ~ONSConsumerACKV8();
+
+    static void Done(const Nan::FunctionCallbackInfo<v8::Value>& info);
+
+public:
+    void SetInner(ONSConsumerACKInner* _inner)
+    {
+        inner = _inner;
+    }
+
+    void Ack(Action result = Action::CommitMessage)
+    {
+        if(inner)
+        {
+            inner->Ack(result);
+            if(ack_env_v == "true") printf(">>> Inner unrefed: 0x%X\n", inner);
+            inner = NULL;
+        }
+    }
+
+    Action WaitResult()
+    {
+        if(inner) return inner->WaitResult();
+        return Action::CommitMessage;
+    }
+
+private:
+    ONSConsumerACKInner* inner;
+};
+
+#endif
