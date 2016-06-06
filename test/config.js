a412e691e77e0a22137417e475cc9da513c68b27
XadillaX <i@2333.moe>
2016-06-06 13:03:00 +0000

    support for travisci

---
+++ test/config.js
@@ -0,0 +1,22 @@
+/**
+ * XadillaX created at 2016-06-06 20:19:05 With ♥
+ *
+ * Copyright (c) 2016 Souche.com, all rights
+ * reserved.
+ */
+"use strict";
+
+try {
+    module.exports = require("./_config");
+} catch(e) {
+    console.log(e);
+    const env = process.env;
+    
+    module.exports = {
+        accessKey: env.ONS_KEY,
+        secretKey: env.ONS_SECRET,
+        producerId: env.ONS_PRODUCER_ID,
+        consumerId: env.ONS_CONSUMER_ID,
+        topic: env.ONS_TOPIC
+    };
+}
---
+++ test/producer.js
@@ -0,0 +1,51 @@
+/**
+ * XadillaX created at 2015-12-19 23:29:10 With ♥
+ *
+ * Copyright (c) 2015 Souche.com, all rights
+ * reserved.
+ */
+"use strict";
+
+const should = require("should");
+
+const config = require("./config");
+const Producer = require("../").Producer;
+
+describe("#producer", function() {
+    const producer = new Producer(config.producerId, config.accessKey, config.secretKey);
+    const date = new Date();
+
+    it("should start producer", function(done) {
+        this.timeout(0);
+        producer.start(done);
+    });
+
+    it("should send an immediate message", function(done) {
+        producer.send(
+            config.topic,
+            "tagA",
+            `Hello ${date.getUTCFullYear()}${date.getUTCMonth()}${date.getUTCDate()}`,
+            function(err, id) {
+                should.ifError(err);
+                id.should.match(/^[0-9A-Z]{32}$/);
+                done();
+            });
+    });
+
+    it("should send a timing message", function(done) {
+        producer.send(
+            config.topic,
+            "tagA",
+            `World ${date.getUTCFullYear()}${date.getUTCMonth()}${date.getUTCDate()}`,
+            function(err, id) {
+                should.ifError(err);
+                id.should.match(/^[0-9A-Z]{32}$/);
+                done();
+            });
+    });
+
+    it("should stop producer", function(done) {
+        producer.stop();
+        done();
+    });
+});
