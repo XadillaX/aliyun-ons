186813026388be9b3b7521aaaeabe4243f547eb8
XadillaX <i@2333.moe>
2016-09-09 06:04:26 +0000

    chore: update ONS CPP dependency

---
+++ src/third_party/include/ONSCallback.h
@@ -0,0 +1,17 @@
+#ifndef __ONSCALLBACK_H__
+#define __ONSCALLBACK_H__
+
+#include "SendResultONS.h"
+#include "ONSClientException.h"
+
+namespace ons {
+class SendCallbackONS
+{
+public:
+    virtual ~SendCallbackONS() {}
+    virtual void onSuccess(SendResultONS& sendResult) = 0;
+    virtual void onException(ONSClientException& e) = 0;
+};
+
+}   //end of namespace SendResultONS
+#endif //end of _SENDCALLBACK_H_
--- src/third_party/include/Producer.h
+++ src/third_party/include/Producer.h
@@ -2,6 +2,7 @@
 #define __PRODUCER_H__
 
 
+#include "ONSCallback.h"
 #include "SendResultONS.h"
 #include "Message.h"
 
@@ -21,6 +22,9 @@ public:
     virtual void shutdown()=0;
     //retry max 3 times if send failed. if no exception throwed, it sends success; 
     virtual SendResultONS send(Message& msg)=0;
+
+    //async send
+    virtual void send(Message& msg,SendCallbackONS* callback) = 0;
 
 };
 
