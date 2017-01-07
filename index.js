d5954aeade8b74f72a6378aaed50d4181a85bc09
XadillaX <admin@xcoder.in>
2017-01-07 16:48:29 +0800

    refactor: update index.js's copyright

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * XadillaX created at 2015-12-19 23:48:21 With ♥
  *
- * Copyright (c) 2015 Souche.com, all rights
+ * Copyright (c) 2017 Souche.com, all rights
  * reserved.
  */
 "use strict";
